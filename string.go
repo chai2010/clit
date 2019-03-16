0d2b32c13fe5f5a621fe812753ad4d7160a7aaed
chai2010 <chaishushan@gmail.com>
2019-03-16 11:20:25 +0800

    zz

--- string.go
+++ string.go
@@ -4,6 +4,56 @@
 
 package clit
 
+import (
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
 func String(s string) string {
-	return "" // todo
+	var sb strings.Builder
+	for _, c := range s {
+		if c > 127 {
+			buf := make([]byte, 5)
+			n := utf8.EncodeRune(buf, c)
+			for i := 0; i < n; i++ {
+				sb.WriteString(`\x`)
+				sb.WriteString(strconv.FormatInt(int64(buf[i]), 16))
+			}
+			continue
+		}
+
+		switch c {
+		case 0:
+			sb.WriteString(`\0`)
+		case '?':
+			sb.WriteString(`\?`)
+
+		case '\a':
+			sb.WriteString(`\a`)
+		case '\b':
+			sb.WriteString(`\b`)
+		case '\f':
+			sb.WriteString(`\f`)
+		case '\n':
+			sb.WriteString(`\n`)
+		case '\r':
+			sb.WriteString(`\r`)
+		case '\t':
+			sb.WriteString(`\t`)
+		case '\v':
+			sb.WriteString(`\v`)
+		case '\\':
+			sb.WriteString(`\\`)
+		case '\'':
+			sb.WriteString(`\'`)
+		case '"':
+			sb.WriteString(`\"`)
+
+		default:
+			sb.WriteRune(c)
+		}
+	}
+
+	return sb.String()
 }
